6e7ec9a38df27aeef2e4a1c42db2a0c480b75226
Tarciso Jr <tarciso.junior@gmail.com>
2019-01-19 03:24:26 -0200

    Initial Commit

---
+++ SegmentedViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentedViews
+//
+//  Created by Tarciso Martins Júnior on 19/01/19.
+//  Copyright © 2019 Tarciso Martins Júnior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
